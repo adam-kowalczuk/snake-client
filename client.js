084857c5f3801bb5f2f9ba90b5f6ce109f5353d5
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-15 17:33:22 -0700

    Removed redundant console log.

--- client.js
+++ client.js
@@ -7,14 +7,23 @@ const connect = function() {
     host: "165.227.47.243"
   });
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: AK");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 500);
+    // setInterval(() => {
+    //   conn.write("Move: left");
+    // }, 1000);
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1500);
+  });
+
   conn.on('data', () => {
     console.log(`you ded cuz you idled`);
   });
-
-  conn.on('connect', () => {
-    console.log("Successfully connected to game server");
-    conn.write("Name: AK")
-  })
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -22,7 +31,6 @@ const connect = function() {
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
-
-module.exports = connect;
+module.exports = {
+  connect
+};
